8a551e782b330e979163554a00e2bd3592ef7d9a
sigmaJEM <joao.marcal12@hotmail.com>
2015-03-09 21:22:46 +0000

    Ta a dar 7 vou mudar o visitor

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ void initVertex( vertex* i){
 
 int hasConection ( node* person, int conection){
   node* iterator;
+  iterator = NULL;
   iterator = person;
   while(iterator != NULL){
     if (iterator->valor == conection){
@@ -20,6 +21,7 @@ int hasConection ( node* person, int conection){
 
 void visitor (vertex* ver, vertex* people, int* max){
   node *nodeIterator;
+  nodeIterator = NULL;
   /* printf("estou no visitor crl %d ->\n", ver->id); */
   if(ver != NULL){
     nodeIterator = ver->conections;
@@ -30,16 +32,14 @@ void visitor (vertex* ver, vertex* people, int* max){
 	people[(nodeIterator->valor)-1].color = 1;
 	people[(nodeIterator->valor)-1].distance = (ver->distance) + 1;
       }
-      if(people[(nodeIterator->valor)-1].distance > (ver->distance) + 1){
+      if(people[(nodeIterator->valor)-1].distance > (ver->distance) + 1 && people[(nodeIterator->valor)-1].color != 0){
 	people[(nodeIterator->valor)-1].distance = (ver->distance) + 1;
       }
-      if(people[(nodeIterator->valor)-1].distance > *(max)){
-	*(max) = people[(nodeIterator->valor)-1].distance;
-      }
+     
       nodeIterator = nodeIterator->next;
     }
     ver->color = 2;
-    
+    nodeIterator = NULL;
     nodeIterator = ver-> conections;
    
     while(nodeIterator != NULL && ver->color == 2  ){
@@ -61,89 +61,101 @@ int main(){
   vertex erdos;
   vertex *people;
   int *result, max;
-  node *conection1, *conection2;
-  nbVertex = nbEdges = max = 0;
+  node *fir, *sec;
+  nbVertex = nbEdges = max = i = 0;
   first = second = 0;
+  fir = sec = NULL;
+  people = NULL;
 
   initVertex( &erdos);
   if( scanf("%d %d %d", &nbVertex, &nbEdges, &erdos.id) == 3){
-    /* printf("nbVertex: %d nbEdges: %d Erdos: %d\n", nbVertex, nbEdges, erdos.id); */
-    
+    printf("nbVertex: %d nbEdges: %d Erdos: %d\n", nbVertex, nbEdges, erdos.id); 
+
 
     people = (vertex*) malloc(sizeof(vertex)*nbVertex);
+
+    for(i = 0; i<nbVertex; i++){
+      initVertex(&(people[i]));
+    }
+
     people[(erdos.id) -1] = erdos;
     
     while( scanf("%d %d ", &first, &second) != EOF ){
-      conection1 = (node*) malloc(sizeof(node));
-      conection2 = (node*) malloc(sizeof(node));
+      fir = (node*) malloc(sizeof(node));
+      sec = (node*) malloc(sizeof(node));
 
-      conection1->valor = second;
-      conection2->valor = first;
+      fir->next = NULL;
+      sec->next= NULL;
+
+      sec->valor = second;
+      fir->valor = first;
 
       if(people[first-1].id == 0){
-	initVertex( &(people[first-1]));
 	people[first-1].id = first;
       }
       if(people[second-1].id == 0){
-	initVertex( &(people[second-1]));
 	people[second-1].id = second;
       }
       
       if(people[first-1].conections != NULL){
-	if(conection1->valor != erdos.id){ 
+	if(sec->valor != erdos.id && sec->valor != people[first-1].id){ 
 	  if((hasConection(people[first-1].conections, second)) == 0){
-	    (people[first-1].endConections)->next = conection1;
-	    people[first-1].endConections = conection1;
+	    (people[first-1].endConections)->next = sec;
+	    people[first-1].endConections = sec;
 	  }
 	}
       }
       if(people[second-1].conections != NULL){
-	if(conection2->valor != erdos.id){
+	if(fir->valor != erdos.id && fir->valor != people[second-1].id){
 	  if((hasConection(people[second-1].conections, first)) == 0){
-	    (people[second-1].endConections)->next = conection2;
-	    people[second-1].endConections = conection2;
+	    (people[second-1].endConections)->next = fir;
+	    people[second-1].endConections = fir;
 	  }
 	}
       }
      
       if(people[first-1].conections == NULL){
-	if(conection1->valor != erdos.id){ 
-	  people[first-1].conections = conection1;
-	  people[first-1].endConections = conection1;
+	if(sec->valor != erdos.id && sec->valor != people[first-1].id ){ 
+	  people[first-1].conections = sec;
+	  people[first-1].endConections = sec;
 	} 
       }
       if(people[second-1].conections == NULL){
-	if(conection2->valor != erdos.id){
-	  people[second-1].conections = conection2;
-	  people[second-1].endConections = conection2; 
+	if(fir->valor != erdos.id && fir->valor != people[second-1].id){
+	  people[second-1].conections = fir;
+	  people[second-1].endConections = fir; 
 	}
       }
     }
 
     visitor(&people[erdos.id -1], people, &max);
-
-    /* for( i = 0; i<=nbVertex-1; i++){
+    /* 
+    for( i = 0; i<=nbVertex-1; i++){
       printf("%d : %d : %d : %d\n", people[i].id, people[i].conections->valor, people[i].distance, people[i].color);
-    }*/
-    
+    }
+    */
+    for( i = 0; i<nbVertex; i++){
+      if(people[i].distance > max){
+        max = people[i].distance;
+      }
+    }
     result = (int*) malloc(sizeof(int)*(max+1));
     result[0] = max;
-    for( i = 0; i<=nbVertex-1; i++){
+    for( i = 0; i<nbVertex; i++){
       if(people[i].distance != 0){
 	result[people[i].distance] += 1;
       }
-      conection1 = people[i].conections;
-      while( conection1 != NULL){
-	conection2 = conection1;
-	conection1 = conection1-> next;
-	free(conection2);
+      sec = people[i].conections;
+      while( sec != NULL){
+	fir = sec;
+	free(fir);
+	sec = sec-> next;
       }
     }
-
     for( i = 0; i<=max; i++){
       printf("%d\n", result[i]);
     }
-       
+     
     free(people);
     free(result);
   }  
