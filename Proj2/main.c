9dd18850ce7108683a2180bb6cbe4ec2ea6e0785
sigmaJEM <joao.marcal12@hotmail.com>
2015-04-21 10:53:44 +0100

    bellman ford quase a funcionar falta por os elementos em A no inicio, falta qd acabamos o bellman ford ver os ciclos negativos com o A e finalizar

--- Proj2/main.c
+++ Proj2/main.c
@@ -14,32 +14,77 @@ void fillNode( node* n, int vertexNb, int cost){
   n-> next = NULL;
 }
 
-void realax(vertex* u, vertex* v ,int  w, int* changeFlag ){
+int qempty(int* head, int *tail){
+  return *(head) == *(tail);
+}
+
+void enqueue(int *q, int x, int* tail, int N){
+  q[*(tail)] = x;
+  if (*(tail) == N)      
+    *(tail) = 0;
+  else
+    (*(tail))++;
+}
+
+void nextQueue(int* head, int N){
+  if(*(head) < N)
+    *(head)++;
+  else
+    *(head)=0;
+}
+
+void relax(vertex* u, vertex* v ,int  w, int* changeFlag, int* queueB, int* tailOfB, int vertexNb){
   if(v -> vertexWeight > u -> vertexWeight + w){
     v -> vertexWeight = u -> vertexWeight + w;
     v -> predecessor = u -> id;
+    endQueue(queueB, v->id, tailOfB, vertexNb);
     changeFlag = 1;
   }
 }
 
-void bellmanFord (vertex* vertexs, int source){
-  
-
+void bellmanFord (vertex* vertexs, int source, int nbVertex, int* queueA, int* queueB, int vertexNb){
+  int changeFlag, headOfA, headOfB, tailOfA, tailOfB;
+  changeFlag = headOfB = headOfA = tailOfB = tailOfA = 0;
+  for (i = 0; i<nbVertex; i++){
+    while (!qempty(&headOfA, &tailOfA)){
+      
+      auxPointer = vertexs[queueA[headOfA]-1].conections;
+      if(vertexs[queueA[headOfA]-1].vertexWeight < INT_MAX/2){
+	while(auxPointer != NULL){
+	  relax(&(vertexs[head-1]), &(vertex[(auxPointer->vertexNb)-1]), auxPointer->cost, &(changeFlag), queueB, &(tailOfB), vertexNb); 
+	  auxPointer = auxPointer->next;
+	}
+      }
+      nextQueue(&(headOfA), vertexNb);
+    
+    }
+    free (queueA);
+    queueA = queueB;
+    queueB = (int*) calloc(vertexNb, sizeof(int));
+    headOfA= 0;
+    tailOfA = tailOfB;
+    tailOfB = headOfB = 0;
+    if(changeFlag = 0){
+      free (queueB);
+      break;
+    }
+  }
 }
 
 int main(){
 
-  int nbVertex, nbEdges, source, beggining, end, cost, i, changeFlag;
+  int nbVertex, nbEdges, source, beggining, end, cost, i;
   vertex *vertexs;
   node *connectionToAdd;
-  nbVertex = nbEdges = source = beggining = end = cost = i = changeFlag = 0;
+  nbVertex = nbEdges = source = beggining = end = cost = i = 0;
   vertexs = NULL;
 
 
   if(scanf("%d %d %d", &nbVertex, &nbEdges, &source) == 3){
 
     vertexs = (vertex*) malloc(sizeof(vertex)*nbVertex);
-    printf("%d %d %d\n", nbVertex,nbEdges, source);
+    queueA = (int*) calloc(nbVertex, sizeof(int));
+    queueB = (int*) calloc(nbVertex, sizeof(int));
 
     for(i = 0; i<nbVertex; i++){
       initVertex(&(vertexs[i]));
